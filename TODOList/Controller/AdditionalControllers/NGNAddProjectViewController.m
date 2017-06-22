877b78344130f34214470c349de49aa55c2d9d60
Alex Stafeyev <noegon@yandex.ru>
2017-06-23 02:46:23 +0300

    [HOTFIX]Notifications were corrected and removed in dealloc, sections number methods were removed in classes where there only one section

--- TODOList/Controller/AdditionalControllers/NGNAddProjectViewController.m
+++ TODOList/Controller/AdditionalControllers/NGNAddProjectViewController.m
@@ -40,10 +40,6 @@
 
 #pragma mark - Table view data source
 
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    return 1;
-}
-
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     return 1;
 }
--- TODOList/Controller/AdditionalControllers/NGNTaskListDetailsViewController.m
+++ TODOList/Controller/AdditionalControllers/NGNTaskListDetailsViewController.m
@@ -52,10 +52,6 @@
 
 #pragma mark - Table view data source
 
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    return 1;
-}
-
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
 //    NGNTaskList *currentTaskList = [NGNTaskService sharedInstance].entityCollection[section];
     return [self.entringTaskList entityCollection].count;
